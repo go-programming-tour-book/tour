6ae1d00cd67f85a0e13f79d70793a7ee439f4d69
EDDYCJY <313687982@qq.com>
2020-04-30 13:09:46 +0800

    修改为大写驼峰

--- cmd/word.go
+++ cmd/word.go
@@ -9,11 +9,11 @@ import (
 )
 
 const (
-	MODE_UPPER                         = iota + 1 // 全部转大写
-	MODE_LOWER                                    // 全部转小写
-	MODE_UNDERSCORE_TO_UPPER_CAMELCASE            // 下划线转大写驼峰
-	MODE_UNDERSCORE_TO_LOWER_CAMELCASE            // 下线线转小写驼峰
-	MODE_CAMELCASE_TO_UNDERSCORE                  // 驼峰转下划线
+	ModeUpper                      = iota + 1 // 全部转大写
+	ModeLower                                 // 全部转小写
+	ModeUnderscoreToUpperCamelCase            // 下划线转大写驼峰
+	ModeUnderscoreToLowerCamelCase            // 下线线转小写驼峰
+	ModeCamelCaseToUnderscore                 // 驼峰转下划线
 )
 
 var str string
@@ -34,15 +34,15 @@ var wordCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var content string
 		switch mode {
-		case MODE_UPPER:
+		case ModeUpper:
 			content = word.ToUpper(str)
-		case MODE_LOWER:
+		case ModeLower:
 			content = word.ToLower(str)
-		case MODE_UNDERSCORE_TO_UPPER_CAMELCASE:
+		case ModeUnderscoreToUpperCamelCase:
 			content = word.UnderscoreToUpperCamelCase(str)
-		case MODE_UNDERSCORE_TO_LOWER_CAMELCASE:
+		case ModeUnderscoreToLowerCamelCase:
 			content = word.UnderscoreToLowerCamelCase(str)
-		case MODE_CAMELCASE_TO_UNDERSCORE:
+		case ModeCamelCaseToUnderscore:
 			content = word.CamelCaseToUnderscore(str)
 		default:
 			log.Fatalf("暂不支持该转换模式，请执行 help word 查看帮助文档")
